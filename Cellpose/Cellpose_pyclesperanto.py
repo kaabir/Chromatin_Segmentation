dc074e74413209e2f7a76059103267f598143d18
Kaabir <38142420+kaabir@users.noreply.github.com>
2023-04-10 23:21:10 +0200

    Chromocenter Segmentation Cases

--- Cellpose/Cellpose_pyclesperanto.py
+++ Cellpose/Cellpose_pyclesperanto.py
@@ -97,15 +97,10 @@ merged_Labels = cle.connected_components_labeling_box(mask)
 statistics = cle.statistics_of_labelled_pixels(img, merged_Labels)
 
 # Conversion to um
-px_to_um_X = 0.0351435 
+px_to_um_X = 0.0351435
 px_to_um_Y = 0.0351435
-nuclei_Area = statistics['area']*px_to_um_Y*px_to_um_Y
-
-intensity_vector = cle.read_intensities_from_map(img,merged_Labels)
-intensity_vector_1_voxel = cle.read_intensities_from_map(img,mask)
-
-#statistics_df = pd.DataFrame(statistics)
-#statistics_df.head()
+px_to_um_Z = 1
+nuclei_Area = statistics['area']*px_to_um_Y*px_to_um_Y*px_to_um_Z
 
 # Chromocenter Segmentation
 # Get voxel of pixel intensity values inside the mask 
@@ -113,40 +108,18 @@ def intensity_vector(mask, img):
     if not (mask.shape == img.shape):
         return False
     
-    mat_intensity = np.where(np.logical_and(mask,img),img,0)
+    mat_intensity = np.where(np.logical_and(mask,img),img,0) # Overlap Mask on original image (as reference)
     return mat_intensity
 
 intensity_map= intensity_vector(mask, img)
 
-mean_Intensity = statistics['mean_intensity']
-stD_Intensity = statistics['standard_deviation_intensity']
-factor = 2.5
-threshold_Chromo = mean_Intensity + factor * stD_Intensity
+from scipy import ndimage
+from scipy.ndimage import gaussian_filter
 
-# Regions of intensity greater than mean/threshold value
-def Chromo_vector(intensity_vector, threshold_value):
-    mat_chromo = np.where(intensity_vector>threshold_value,intensity_vector,0)
-    return mat_chromo
-
-mat_intensity = Chromo_vector(intensity_map,threshold_Chromo)
-segmented_chromo = cle.gaussian_blur(mat_intensity, sigma_x=2, sigma_y=2)
-segmented_chromo = cle.threshold_otsu(segmented_chromo)
-
-# Merge Segmented Mask
-chromo_Labels = cle.connected_components_labeling_box(segmented_chromo) #Chromo are seperate
-# Getting stats
-statistics_chromo = cle.statistics_of_labelled_pixels(img, chromo_Labels)
-chromo_Area = np.sum(statistics_chromo['area'], axis=0)
-chromo_Area = chromo_Area * px_to_um_Y*px_to_um_X
-
-# Max Entropy Chromocenter Segmentation -> Orginial Voxel -> Blur (rx=ry=2) -> Max(Entropy Threshold) 
-intensity_map_blur = cle.gaussian_blur(intensity_map, sigma_x=2, sigma_y=2)
-entropy_Threshold_Chromo = nsitk.threshold_maximum_entropy(intensity_map_blur)
-
-# Sort Nuclei Label Mask
 #print(len(np.unique(merged_Labels)))
 number_of_Nuclei = len(np.unique(merged_Labels))-1
 
+# Create a matrice to place the single nuleus with intensity voxel
 im_obj1 = np.zeros(img.shape) 
 im_obj2 = np.zeros(img.shape) 
 im_obj3 = np.zeros(img.shape)
@@ -154,7 +127,8 @@ nuc_lbl_lst1 = []
 nuc_lbl_lst2 = []
 nuc_lbl_lst3 = []
 
-for i in np.unique(merged_Labels_np)[1:]: # (1) is BG mask
+# Extracting mask for each nucleus intensity and storing them
+for i in np.unique(merged_Labels_np)[1:]: # Initial (1) is mask
     if (len(np.unique(merged_Labels)-1) == 1):
         break
     elif i ==1:
@@ -178,14 +152,16 @@ for i in np.unique(merged_Labels_np)[1:]: # (1) is BG mask
         #viewer.add_image(im_obj3)
 
     else:
-        print(' More than four nuclei...')    
+        raise ValueError('More than three nucleus')   
+    # viewer = napari.Viewer()
+    # viewer.add_image(y)
 
 # Not replacing back the original mask values associted with labels
 # Place back the intensity values to the individual label
 def chromocenter_vector(mask, img):
     if not (mask.shape == img.shape):
         return False
-      
+
     chromocenter_intensity = np.where(np.logical_and(mask,img),img,0) # Overlap Mask on original image (as reference)
     return chromocenter_intensity
 
@@ -202,20 +178,50 @@ for i in np.unique(merged_Labels_np)[1:]: # Background mask at 1
         break
     nuc_lbl_lst1 = np.vstack(nuc_lbl_lst1)
     intensity_map1= chromocenter_vector(nuc_lbl_lst1, img)
-    # Apply Thresholding for Chromocenters
-    intensity_map_blur1 = nsitk.gaussian_blur(intensity_map1, variance_x=2, variance_y=2)
-    intermodes_Threshold_Chromo1 = nsitk.threshold_intermodes(intensity_map_blur1)
-    # Get Statistics
-    chromo_Labels1 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo1)
-    statistics_chromo1 = cle.statistics_of_labelled_pixels(img, chromo_Labels1)
-    # Caculate Area
-    chromo_Area1 = np.sum(statistics_chromo1['area'], axis=0)
-    chromo_Area1 = chromo_Area1 * px_to_um_Y*px_to_um_X
     
+    # Get Nucleus Statistics
+    statistics_nucleus1 = cle.statistics_of_labelled_pixels(img, nuc_lbl_lst1)
+    pd.DataFrame(statistics_nucleus1).to_excel(Result_folder+filename+'(Nucleus)Nucleus_statistics_1.xlsx')
+ 
+    # Blur to enhance chromocenters
+    intensity_map_blur1 = nsitk.gaussian_blur(intensity_map1, variance_x=2, variance_y=2)
+    # Apply Intermodes Thresholding for Chromocenters
+    intermodes_Threshold_Chromo1 = nsitk.threshold_intermodes(intensity_map_blur1)
+    # Caculate intermodes Area
+    chromo_intermodes_Labels1 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo1)
+    statistics_intermodes_chromo1 = cle.statistics_of_labelled_pixels(img, chromo_intermodes_Labels1)
+    
+    chromointermodes_Area1 = np.sum(statistics_intermodes_chromo1['area'], axis=0)
+    chromointermodes_Area1 = chromointermodes_Area1 * px_to_um_Y*px_to_um_X
+    
+    # Max Entropy Chromocenter Segmentation -> Orginial Voxel -> Blur (rx=ry=2) -> Max(Entropy Threshold) 
+    entropy_Threshold_Chromo1 = nsitk.threshold_maximum_entropy(intensity_map_blur1)
+    chromo_entropy_Labels1 = cle.connected_components_labeling_box(entropy_Threshold_Chromo1)
+    # Caculate Entropy Area
+    statistics_entropy_chromo1 = cle.statistics_of_labelled_pixels(img, chromo_entropy_Labels1)
+    
+    chromoentropy_Area1 = np.sum(statistics_entropy_chromo1['area'], axis=0)
+    chromoentropy_Area1 = chromoentropy_Area1 * px_to_um_Y*px_to_um_X
+    
+    #binary_fill_holes = binary_fill_holes(intermodes_Threshold_Chromo1)
+    #exclude_tiny_label = cle.exclude_small_labels(binary_fill_holes, maximum_size = 500)
+    #exclude_tiny_label = cle.exclude_large_labels(binary_fill_holes, minimum_size = 40)
+    
+    # Export chromocenter statistics based on right range achieved out of both segmentation
+    if chromointermodes_Area1 > 10 and chromoentropy_Area1 > 10:
+        raise ValueError('Chromocenter Segmentation Fault in Chromocenter One')
+    elif chromointermodes_Area1 > 10:
+         pd.DataFrame(statistics_entropy_chromo1).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_1.xlsx')
+    elif chromoentropy_Area1 > 10:
+         pd.DataFrame(statistics_intermodes_chromo1).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_1.xlsx')
+
     # Check the nuclei
     viewer = napari.Viewer()
+    #viewer.add_image(binary_fill_holes)
     viewer.add_image(intensity_map1)
-    viewer.add_image(chromo_Labels1)
+    viewer.add_image(chromo_entropy_Labels1)
+    viewer.add_image(chromo_intermodes_Labels1)
+    
     # Nuclei 2 to be segmented for chromocenter 2
     if not empty_nuclei(nuc_lbl_lst2):
         break
@@ -223,44 +229,96 @@ for i in np.unique(merged_Labels_np)[1:]: # Background mask at 1
     intensity_map2= chromocenter_vector(nuc_lbl_lst2, img) 
     
     intensity_map_blur2 = nsitk.gaussian_blur(intensity_map2, variance_x=2, variance_y=2)
-    intermodes_Threshold_Chromo2 = nsitk.threshold_intermodes(intensity_map_blur2)  
+    intermodes_Threshold_Chromo2 = nsitk.threshold_intermodes(intensity_map_blur2)
+    # Get Nucleus Statistics
+    statistics_nucleus2 = cle.statistics_of_labelled_pixels(img, nuc_lbl_lst2)
+    pd.DataFrame(statistics_nucleus2).to_excel(Result_folder+filename+'(Nucleus)Nucleus_statistics_2.xlsx')
+ 
+    # Blur to enhance chromocenters
+    intensity_map_blur2 = nsitk.gaussian_blur(intensity_map2, variance_x=2, variance_y=2)
+    # Apply Intermodes Thresholding for Chromocenters
+    intermodes_Threshold_Chromo2 = nsitk.threshold_intermodes(intensity_map_blur2)
+    # Caculate intermodes Area
+    chromo_intermodes_Labels2 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo2)
+    statistics_intermodes_chromo2 = cle.statistics_of_labelled_pixels(img, chromo_intermodes_Labels2)
+    
+    chromointermodes_Area2 = np.sum(statistics_intermodes_chromo2['area'], axis=0)
+    chromointermodes_Area2 = chromointermodes_Area2 * px_to_um_Y*px_to_um_X
+    
+    # Max Entropy Chromocenter Segmentation -> Orginial Voxel -> Blur (rx=ry=2) -> Max(Entropy Threshold) 
+    entropy_Threshold_Chromo2 = nsitk.threshold_maximum_entropy(intensity_map_blur2)
+    chromo_entropy_Labels2 = cle.connected_components_labeling_box(entropy_Threshold_Chromo2)
+    # Caculate Entropy Area
+    statistics_entropy_chromo2 = cle.statistics_of_labelled_pixels(img, chromo_entropy_Labels2)
+    
+    chromoentropy_Area2 = np.sum(statistics_entropy_chromo2['area'], axis=0)
+    chromoentropy_Area2 = chromoentropy_Area2 * px_to_um_Y*px_to_um_X
+    
+    #binary_fill_holes = binary_fill_holes(intermodes_Threshold_Chromo2)
+    #exclude_tiny_label = cle.exclude_small_labels(binary_fill_holes, maximum_size = 500)
+    #exclude_tiny_label = cle.exclude_large_labels(binary_fill_holes, minimum_size = 40)
+    
+    # Export chromocenter statistics based on right range achieved out of both segmentation
+    if chromointermodes_Area2 > 10 and chromoentropy_Area2 > 10:
+        raise ValueError('Chromocenter Segmentation Fault in Chromocenter two')
+    elif chromointermodes_Area2 > 10 :
+        pd.DataFrame(statistics_entropy_chromo2).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_2.xlsx')
+    elif chromoentropy_Area2 > 10:
+        pd.DataFrame(statistics_intermodes_chromo2).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_2.xlsx')
 
-    # Get Statistics
-    chromo_Labels2 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo2)
-    statistics_chromo2 = cle.statistics_of_labelled_pixels(img, chromo_Labels2)     
-    # Caculate Area
-    chromo_Area2 = np.sum(statistics_chromo2['area'], axis=0)
-    chromo_Area2 = chromo_Area2 * px_to_um_Y*px_to_um_X
     # Check the nuclei
     viewer = napari.Viewer()
     viewer.add_image(intensity_map2)
-    viewer.add_image(chromo_Labels2)
+    viewer.add_image(chromo_entropy_Labels2)
+    viewer.add_image(chromo_intermodes_Labels2)
+
     # Nuclei 3 to be segmented for chromocenter 3
     if not empty_nuclei(nuc_lbl_lst3):
         break
     nuc_lbl_lst3 = np.vstack(nuc_lbl_lst3)
-    intensity_map3= chromocenter_vector(nuc_lbl_lst3, img)  
-    
-    intensity_map_blur3 = cle.gaussian_blur(intensity_map3, sigma_x=2, sigma_y=2)
+    intensity_map3= chromocenter_vector(nuc_lbl_lst3, img) 
+
+    intensity_map_blur3 = nsitk.gaussian_blur(intensity_map3, variance_x=2, variance_y=2)
     intermodes_Threshold_Chromo3 = nsitk.threshold_intermodes(intensity_map_blur3)
-    
-    # Get Statistics
-    chromo_Labels3 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo3)
-    statistics_chromo3 = cle.statistics_of_labelled_pixels(img, chromo_Labels3)     
-    # Caculate Area
-    chromo_Area3 = np.sum(statistics_chromo3['area'], axis=0)
-    chromo_Area3 = chromo_Area3 * px_to_um_Y*px_to_um_X
+    # Get Nucleus Statistics
+    statistics_nucleus2 = cle.statistics_of_labelled_pixels(img, nuc_lbl_lst3)
+    pd.DataFrame(statistics_nucleus2).to_excel(Result_folder+filename+'(Nucleus)Nucleus_statistics_2.xlsx')
+
+    # Blur to enhance chromocenters
+    intensity_map_blur3 = nsitk.gaussian_blur(intensity_map3, variance_x=2, variance_y=2)
+    # Apply Intermodes Thresholding for Chromocenters
+    intermodes_Threshold_Chromo3 = nsitk.threshold_intermodes(intensity_map_blur3)
+    # Caculate intermodes Area
+    chromo_intermodes_Labels3 = cle.connected_components_labeling_box(intermodes_Threshold_Chromo3)
+    statistics_intermodes_chromo3 = cle.statistics_of_labelled_pixels(img, chromo_intermodes_Labels3)
+
+    chromointermodes_Area3 = np.sum(statistics_intermodes_chromo3['area'], axis=0)
+    chromointermodes_Area3 = chromointermodes_Area3 * px_to_um_Y*px_to_um_X
+
+    # Max Entropy Chromocenter Segmentation -> Orginial Voxel -> Blur (rx=ry=2) -> Max(Entropy Threshold) 
+    entropy_Threshold_Chromo3 = nsitk.threshold_maximum_entropy(intensity_map_blur3)
+    chromo_entropy_Labels3 = cle.connected_components_labeling_box(entropy_Threshold_Chromo3)
+    # Caculate Entropy Area
+    statistics_entropy_chromo3 = cle.statistics_of_labelled_pixels(img, chromo_entropy_Labels3)
+
+    chromoentropy_Area3 = np.sum(statistics_entropy_chromo3['area'], axis=0)
+    chromoentropy_Area3 = chromoentropy_Area3 * px_to_um_Y*px_to_um_X
+
+    #binary_fill_holes = binary_fill_holes(intermodes_Threshold_chromo3)
+    #exclude_tiny_label = cle.exclude_small_labels(binary_fill_holes, maximum_size = 500)
+    #exclude_tiny_label = cle.exclude_large_labels(binary_fill_holes, minimum_size = 40)
+
+    # Export chromocenter statistics based on right range achieved out of both segmentation
+    if chromointermodes_Area3 > 10 and chromoentropy_Area3 > 10:
+        raise ValueError('Chromocenter Segmentation Fault in Chromocenter two')
+    elif chromointermodes_Area3 > 10 :
+        pd.DataFrame(statistics_entropy_chromo3).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_2.xlsx')
+    elif chromoentropy_Area3 > 10:
+        pd.DataFrame(statistics_intermodes_chromo3).to_excel(Result_folder+filename+'(Chromo)Chromo_statistics_2.xlsx')
+
     # Check the nuclei
     viewer = napari.Viewer()
     viewer.add_image(intensity_map3)
-    viewer.add_image(chromo_Labels3)
+    viewer.add_image(chromo_entropy_Labels3)
+    viewer.add_image(chromo_intermodes_Labels3)
 
- # Method running chromocenters segmentation.
- # Simple algorithms description :
- # 1- Gaussian blur
- # 2- Image gradient : for each voxels computing sum of difference with
- # X (_neigh parameter) neighborhood.
- # Thresholding value : keep voxels having value higher mean nucleus
- # intensity plus X (_factor parameter) standard deviation value
- # 4- Binarization of threshold image
- # 5- Connected component computation from binarized image
