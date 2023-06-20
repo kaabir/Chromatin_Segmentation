b6a150e85777692a021629f9bf31b953dc7a07f0
Kaabir <38142420+kaabir@users.noreply.github.com>
2023-06-20 18:56:25 +0200

    Add output export

--- Cellpose/Cellpose_staining_2d_slices.py
+++ Cellpose/Cellpose_staining_2d_slices.py
@@ -2,13 +2,13 @@
 """
 Created on Wed Mar  8 18:22:51 2023
 
-@author: kaabir
+@author: kaabi
 """
 
 import time, os, sys
 import skimage.io 
 from skimage.io import imread, imshow
-from cellpose import models, utils, plot, io, core
+from cellpose import models, utils, plot, io, core 
 import matplotlib as mpl
 import napari 
 import pyclesperanto_prototype as cle
@@ -23,38 +23,122 @@ from skimage import img_as_float32
 from skimage.util import img_as_ubyte
 from tifffile import imsave
 from napari_segment_blobs_and_things_with_membranes import split_touching_objects
+
+device = cle.select_device("AMD Radeon Pro W6600")
+
+from scipy import ndimage as ndi
+# Structure  for Actin Dilation
+    # Structure  for Actin Dilation
+diamond = np.zeros((3, 3, 3), dtype=bool)
+diamond[1:2, 0:3, 0:3] = True
+
 # model_type='cyto' or model_type='nuclei'
 #model = models.Cellpose(model_type='cyto', gpu = True) #nuclei model for brdu
 # Read the Tif File
 #folder_path = 'C:/Users/NBMS1/Documents/Nuclei Data/Cellpose/Test_Dataset/15-12-22/T30/'
-folder_path = 'C:/Users/ChimieENS/Documents/Cellpose/H3K_Staining/'
+folder_path = 'C:/Users/kaabi/Documents/Nuceli_Data/Enucleation/Cellpose/H3K_Staining/'
 
-file_path = '230413 OF1 D1 Ctrl 10uMVCA Gactin RH3K9me2 FRH3K27me2 01-1.tif'
+file_path = 'Binned_z_3d-20221103_IF_OF1 D1 H3K27me3 FOP FOXJ1_1H_Ctrl_S2 _01.tif'
+#3d-20221103 IF OF1 D1 H3K27me3 FOP FOXJ1 -1H Ctrl -01-1
 img_path = folder_path + file_path
 
-aics_image = AICSImage(img_path)
-img = aics_image.get_image_data("YX", T=0)
-#img = imread(img_path)
+def trim_array(arr):
+    """Returns a trimmed view of an n-D array excluding any outer
+    regions which contain only zeros.
+    https://stackoverflow.com/questions/55917328/numpy-trim-zeros-in-2d-or-3d
+    """
+    slices = tuple(slice(idx.min(), idx.max() + 1) for idx in np.nonzero(arr))
+    return arr[slices]
 
-folder_path_red = '230413 OF1 D1 Ctrl 10uMVCA FRH3K27me2 03.tif'
-img_path_red = folder_path + folder_path_red 
-#img_red = imread(img_path)
-aics_image_red = AICSImage(img_path_red)
-img_red = aics_image_red.get_image_data("YX", T=0)
+def mid_Slice(img_stack):
+    # Find the halfway point of the array along the first axis
+    mid_slice = img_stack.shape[0] // 2
 
-# In case two channels
-# img_actin = img[:,0,:,:] # A Channel
-# img_nuclei = img[:,1,:,:] # N Channel
+    # Calculate the start and end indices for slicing
+    start_index = mid_slice - 2
+    end_index = mid_slice + 2
 
-# In case three channels
-# img_actin = img[:,:,:,0] # A Channel
-# img_dextran = img[:,:,:,1] # Dextran Channel
-# img_nuclei = img[:,:,:,2] # N Channel
-#img = img[:,:,:,-1] # # Hoescht Channel
+    # Slicing the array
+    img_stack = img_stack[start_index:end_index+1, :, :]
+    
+    return img_stack
 
-# get_Zstack = img.shape[0]
-# get_Xresl = img.shape[1]
-# get_yresl = img.shape[2]
+# Read the Tif/CZI File
+def folder_scan(directory):
+    # Actin after 30min - value assigned is 3
+    get_files = []
+    extension = ".czi"  # ".czi"
+    for f_name in os.listdir(directory):
+        if f_name.endswith(extension):
+            get_files.append(os.path.join(directory, f_name))
+    return get_files
+
+def normalize_intensity(k):
+    k_min = k.min()
+    k_max = k.max()
+
+    k = (k - k_min)/(k_max-k_min)
+    k[np.isnan(k)] = 0
+    return k
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 0)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+    
+# Get voxel of pixel intensity values inside the mask 
+def replace_intensity(mask, img):
+    if not (mask.shape == img.shape):
+        return False
+    
+    mat_intensity = np.where(np.logical_and(mask,img),img,0) # Overlap Mask on original image (as reference)
+    return mat_intensity
+
+#folder_path = 'C:/Users/kaabi/Documents/Nuceli_Data/Enucleation/Cellpose/H3K_Staining/'
+
+#get_files = folder_scan(folder_path)
+
+# Conversion to um
+px_to_um_X = 0.0351435
+px_to_um_Y = 0.0351435
+px_to_um_Z = 0.32 #1.0/0.0351435 # making pixels isotropic
+
+
+aics_image = AICSImage(img_path) # Read indivi
+filename = Path(img_path).stem
+# <Dimensions [T: 1, C: 1, Z: 256, Y: 256, X: 256]> # ("ZYX", T=0)
+no_chnls = aics_image.dims.C
+if no_chnls == 1:
+        img_nuclei = aics_image.get_image_data("ZYX", T=0, C=0) 
+elif no_chnls == 2: 
+        img_actin = aics_image.get_image_data("ZYX", T=0, C=0) # A Channel
+        img_nuclei = aics_image.get_image_data("ZYX", T=0, C=1)
+elif no_chnls == 3:
+        img_actin = aics_image.get_image_data("ZYX", T=0, C=0) # A Channel
+        img_dextran_read = aics_image.get_image_data("ZYX", T=0, C=1) # Dextran Channel
+        img_nuclei = aics_image.get_image_data("ZYX", T=0, C=2)
+else:
+        img_fop = aics_image.get_image_data("ZYX", T=0, C=0) # Actin Channel 
+        img_h3k27me3 = aics_image.get_image_data("ZYX", T=0, C=1) # yH2AX
+        img_foxj = aics_image.get_image_data("ZYX", T=0, C=2) # LaminB1
+        img_nuclei = aics_image.get_image_data("ZYX", T=0, C=3) # Nucleus
+
+# img_nuclei = np.max(img_nuclei, axis=0)
+
+# if 'img_fop' in globals():
+#     img_fop = np.max(img_fop, axis=0)
+
+# if 'img_h3k27me3' in globals():
+#     img_h3k27me3 = np.max(img_h3k27me3, axis=0)
+    
+# if 'img_foxj' in globals():
+#     img_foxj = np.max(img_foxj, axis=0)   
+
+if not os.path.exists(folder_path + '/Result'):
+        os.makedirs(folder_path + '/Result')
+        
+Result_folder = folder_path + '/Result/' 
+
 
 # In case four channels
 
@@ -63,6 +147,7 @@ img_red = aics_image_red.get_image_data("YX", T=0)
 # # Hoescht Channel
 # img = img_path.get_image_data("ZYX", C=2, S=0, T=0)
 # get_Zstack = img.shape[0]
+
 
 # For saving results
 filename = Path(file_path).parts[-1]
@@ -77,44 +162,64 @@ Result_folder = folder_path + '/Result/'
 # for your single channel image use [0, 0] 
 # for the multi channel image it's [3, 0]
 channels = [0, 0] 
-diameter = 30 #169.708
+diameter = 90 #169.708
 use_GPU = True
 cellprob_threshold = 0
 
-model_match_threshold = 27#30
+stitch_threshold = 1
+
+model_match_threshold = 25#30
 flow_threshold = (31.0 - model_match_threshold) / 10.0
+
+print('cyto2 begins')
+
+pretrained_model = "C:/Users/kaabi/Documents/Nuceli_Data/Enucleation/Cellpose/models_trained_Nuclei/CP_20230306_150448_200"
 
 logger = io.logger_setup()
 
-model = models.CellposeModel(gpu=use_GPU, model_type='cyto')#,residual_on=False, style_on=False, concatenation=False)#, resample=True, do_3D=True,stitch_threshold=1)
-mask, flows, styles = model.eval(img, 
-                                            channels=channels, 
-                                            diameter=diameter, 
-                                            #anisotropy=anisotropy, 
-                                            do_3D=False, 
-                                            net_avg=True, 
-                                            resample=True,
-                                            #augment=False, 
-                                            cellprob_threshold=0)
+model = models.CellposeModel(gpu=use_GPU, model_type=pretrained_model)#,residual_on=False, style_on=False, concatenation=False)#, resample=True, do_3D=True,stitch_threshold=1)
+mask, flows,styles = model.eval(img_nuclei,
+                                channels =channels,
+                                #anisotropy=anisotropy,
+                                diameter=diameter, 
+                                #pretrained_model=pretrained_model,
+                                do_3D=False,
+                                resample=True,
+                                min_size = 10,
+                                flow_threshold =flow_threshold,
+                                cellprob_threshold = cellprob_threshold,
+                                net_avg=True,
+                                stitch_threshold=stitch_threshold,
+                                #model_match_threshold = model_match_threshold
+                                    )
 
-merged_Labels_np = np.array(mask)
+#mask_np1 = nsitk.morphological_watershed(mask)
+#mask_np = np.array(mask)
+
+clean_mask = cle.exclude_labels_on_edges(mask)
+
+from skimage.segmentation import clear_border
+from scipy.ndimage import label
+
 #merged_Labels_np = split_touching_objects(merged_Labels_np)
 
-    # Check the nuclei
-viewer = napari.Viewer()
-viewer.add_image(img)
-viewer.add_image(merged_Labels_np)
+#composite = np.dstack((img_fop, img_h3k27me3, img_foxj, img_nuclei))
+
+
+# # Perform Z-projection on each channel
+# projection_method = "max"  # Choose the desired projection method ("max", "min", "mean", "sum", etc.)
+# composite = []
+# for channel_index in range(merged_Labels_np):
+#     z_projected_channel = np.max(channel_index, axis=0)  # Replace with the desired projection method
+#     composite.append(z_projected_channel)
 
 # Save Labels
-prediction_stack_32 = img_as_float32(mask, force_copy=False)     
-os.chdir(Result_folder)
+#prediction_stack_32 = img_as_float32(mask, force_copy=False)     
+#os.chdir(Result_folder)
 #imsave(str(filename)+".tif", prediction_stack_32)
 # Getting Original intensities
 #intensity_vector = cle.read_intensities_from_map(mask, img)
 ##statistics_hoescht = cle.statistics_of_labelled_pixels(img, merged_Labels_np)
-statistics_hoescht = nsitk.label_statistics(img, merged_Labels_np)
-
-statistics_h39me2 = cle.statistics_of_labelled_pixels(img_red, merged_Labels_np)
 #
 # Conversion to um
 px_to_um_X = 0.0351435
@@ -123,33 +228,88 @@ px_to_um_Z = 1
 #nuclei_Area = statistics['area']*px_to_um_Y*px_to_um_Y*px_to_um_Z
 
 # Chromocenter Segmentation
-# Get voxel of pixel intensity values inside the mask 
-def intensity_vector(mask, img):
-    if not (mask.shape == img.shape):
-        return False
-    
-    mat_intensity = np.where(np.logical_and(mask,img),img,0) # Overlap Mask on original image (as reference)
-    return mat_intensity
-
-intensity_map= intensity_vector(mask, img)
-
-def normalize_intensity(k):
-    k_min = k.min(axis=(1, 2), keepdims=True)
-    k_max = k.max(axis=(1, 2), keepdims=True)
-
-    k = (k - k_min)/(k_max-k_min)
-    k[np.isnan(k)] = 0
-    return k
-
-from scipy import ndimage
-from scipy.ndimage import gaussian_filter
 
 
-# Create a matrice to place the single nuleus with intensity voxel
-im_obj1 = np.zeros(img.shape) 
-im_obj2 = np.zeros(img.shape) 
-im_obj3 = np.zeros(img.shape)
-nuc_lbl_lst1 = []
-nuc_lbl_lst2 = []
-nuc_lbl_lst3 = []
+from skimage import measure
+labels = measure.label(clean_mask)
 
+for i, label in enumerate(np.unique(labels)[1:]):
+    print('i ---', i)
+        #print('label ---', label)
+    if label == 15:
+        print('label ---', label)
+            # create a mask for the current label
+            #if (len(np.unique(merged_Labels)-1) == 1):# and no_chnls>1:
+                # break    
+        mask_unique = labels == label
+        #nuc_lbl = np.array(label)
+        #label_crop = trim_array(mask)
+
+        
+        # Dilation of unqiue label
+        dilated_mask = ndi.binary_dilation(mask_unique, structure = diamond, iterations=20).astype(mask_unique.dtype)
+        # FoxJ Channel
+        intensity_foxj = replace_intensity(dilated_mask, img_foxj)
+        foxj_arr = trim_array(intensity_foxj)
+
+        #print('nucelus_arr ---', nucelus_arr.shape)
+        #
+        # Chromocenter Dots
+        normal_nucelus= replace_intensity(mask_unique, img_nuclei)
+        padding_nucleus_arr = trim_array(normal_nucelus)
+        # Get the shape difference between the two arrays
+        shape_diff = np.subtract(foxj_arr.shape,padding_nucleus_arr.shape)
+        shape_diff = int(shape_diff[0]/2)
+        
+        intensity_map_norm = normalize_intensity(normal_nucelus)
+        intensity_map_blur = nsitk.median_filter(intensity_map_norm, radius_x=2, radius_y=2, radius_z=0)
+        intensity_map_thb = cle.top_hat_box(intensity_map_blur, None, 10.0, 10.0, 0.0)
+        
+        try:
+            intermodes_Threshold_Chromo = nsitk.threshold_intermodes(intensity_map_thb)
+        except RuntimeError:
+            print("Oops!  There was a trciky image to segment....")
+        # Threshold isodata
+        # image2_T = nsitk.threshold_isodata(image1_gb)
+        # image3_T = nsitk.threshold_maximum_entropy(image1_gb)
+        
+        # Calculate the padding values for each dimension
+        #padding = [(0, diff) for diff in shape_diff]  # Pad with zeros
+
+        # Pad array1 to match the shape of array2
+        
+        # Normal nucleus
+        
+        ###nucelus_arr = np.pad(padding_nucleus_arr, shape_diff, pad_with)
+        
+        # H3k27me3 Channel
+        intensity_h3k27me3 = replace_intensity(mask_unique, img_h3k27me3)
+        h3k27me3_trim = trim_array(intensity_h3k27me3)
+        h3k27me3_arr = np.pad(h3k27me3_trim, shape_diff, pad_with)
+        #h3k27me3_arr = np.pad(intensity_h3k27me3, padding,  constant_values=0, mode='constant')
+        #intensity_h3k27me3 = replace_intensity(dilated_mask, img_h3k27me3)
+        #h3k27me3_trim = trim_array(intensity_h3k27me3)
+        #h3k27me3_arr = ndi.binary_dilation(h3k27me3_trim, structure = diamond, iterations=2).astype(mask_unique.dtype)
+        
+        # FOP Channel
+        intensity_fop = replace_intensity(mask_unique, img_fop)
+        fop_trim = trim_array(intensity_fop)
+        fop_arr = np.pad(fop_trim, shape_diff, pad_with)
+        #fop_trim = trim_array(intensity_fop)
+        #fop_arr = ndi.binary_dilation(fop_trim, structure = diamond, iterations=2).astype(mask_unique.dtype)
+        
+       ### merged_image = np.stack([fop_arr, h3k27me3_arr, foxj_arr, nucelus_arr], axis=0)
+
+        
+        #print(image3_T.shape)
+        viewer = napari.Viewer()
+        ####viewer.add_image(merged_image)
+        ####viewer.add_image(image2_T)
+        ####viewer.add_image(image3_T)
+        #viewer.add_image(intermodes_Threshold_Chromo)
+        viewer.add_image(img_nuclei)
+        viewer.add_image(mask)
+        viewer.add_image(clean_mask)
+        viewer.add_image(mask_unique)
+        viewer.add_image(intensity_h3k27me3)
+        viewer.add_image(intensity_foxj)
