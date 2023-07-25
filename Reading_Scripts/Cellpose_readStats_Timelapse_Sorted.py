f7c59fd962692f74266949b56482e9b2a826e9d8
Kaabir <38142420+kaabir@users.noreply.github.com>
2023-07-25 15:05:46 +0200

    Match Index Values with Timestamp

--- Reading_Scripts/Cellpose_readStats_Timelapse_Sorted.py
+++ Reading_Scripts/Cellpose_readStats_Timelapse_Sorted.py
@@ -94,22 +94,28 @@ def folder_scan(directory, extension, marker=None):
 
 # Main Folder
 folder_Path = "E:/Quantified/eNUC Live/230120 OF1 D0 Enuc Ctrl5_actin5/"
+###########################
+#   Actin    Reading      #
+###########################
+# Time T0
 directory = folder_Path + 'T0/Result/Actin/'
 
 ChromoFilesT0 = folder_scan(directory, ".xlsx", marker="(Chromo)_")
 NucleusFilesT0 = folder_scan(directory, ".xlsx", marker="(Nucleus)_")
 
+ChromoFilesT0 = sorted(ChromoFilesT0)
+NucleusFilesT0 = sorted(NucleusFilesT0)
+
+# Time T30
 directory = folder_Path + 'T30/Result/Actin/'
 
 ChromoFilesT1 = folder_scan(directory, ".xlsx", marker="(Chromo)_")
 NucleusFilesT1 = folder_scan(directory, ".xlsx", marker="(Nucleus)_")
 ActinFilesT1 = folder_scan(directory, ".xlsx", marker="(Actin)_")
 
-exactChromoFiles =  matchingFiles(ChromoFilesT0, ChromoFilesT1,sortedMarker="(Chromo)_", sourceMarker="(Chromo)_")
-
-exactNucleusFiles =  matchingFiles(NucleusFilesT0, NucleusFilesT1,sortedMarker="(Nucleus)_", sourceMarker="(Nucleus)_")
-
-exactActinFiles =  matchingFiles(NucleusFilesT0, ActinFilesT1,sortedMarker="(Nucleus)_", sourceMarker="(Actin)_")
+ChromoFilesT1 = sorted(ChromoFilesT1)
+NucleusFilesT1 = sorted(NucleusFilesT1)
+ActinFilesT1 = sorted(ActinFilesT1)
 
 # Actin T0
 for nucleusread in NucleusFilesT0:
@@ -118,36 +124,102 @@ for nucleusread in NucleusFilesT0:
     stats_Actin["File_Name_Actin"].append(file_name)
     stats_Actin["Nuclei_volume_actin_T0"].append(df.loc[0,'Nucleus Area'])
     stats_Actin["Nuclei_intensity_actin_T0"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
-
-# Actin T30        
-for nucleusread in exactNucleusFiles:
-    df = pd.read_excel(nucleusread)
-    #file_name = nucleusread.split("(Nucleus)_")[1].split(".")[0].strip()
-    #stats_Actin["File_Name_Actin"].append(file_name)
-    stats_Actin["Nuclei_volume_actin_T1"].append(df.loc[0,'Nucleus Area'])
-    stats_Actin["Nuclei_intensity_actin_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
-        # else:
-           
-# Chromocenter Read
-for temp_read in ChromoFilesT0:
-    df = pd.read_excel(temp_read)        
+    
+# Actin T30             
+for nucleusread in NucleusFilesT1:
+    file_name = nucleusread.split("(Nucleus)_")[1].split(".")[0].strip()
+    file_name = file_name.replace("T30", "T0")
+    
+    if file_name in stats_Actin["File_Name_Actin"]:
+        df = pd.read_excel(nucleusread)
+        stats_Actin["Nuclei_volume_actin_T1"].append(df.loc[0,'Nucleus Area'])
+        stats_Actin["Nuclei_intensity_actin_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
+    # else:
+    #     stats_Actin["File_Name_Actin"].append(file_name)
+    #     stats_Actin["Nuclei_volume_actin_T1"].append(None)
+    #     stats_Actin["Nuclei_intensity_actin_T1"].append(None)
+             
+# Chromocenter Read T0
+for chromo_read in ChromoFilesT0:
+    df = pd.read_excel(chromo_read)        
     stats_Actin["Actin_chromo_volume_T0"].append(df.loc[0, 'Chromocenter Area'])
     stats_Actin["Actin_chromo_intensity_T0"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])     
+# Chromocenter Read T30
+for chromo_read in ChromoFilesT1:
+    file_name = chromo_read.split("(Chromo)_")[1].split(".")[0].strip()
+    file_name = file_name.replace("T30", "T0") 
+    if file_name in stats_Actin["File_Name_Actin"]:
+        df = pd.read_excel(chromo_read)        
+        stats_Actin["Actin_chromo_volume_T1"].append(df.loc[0, 'Chromocenter Area'])
+        stats_Actin["Actin_chromo_intensity_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
+    # else:
+    #     stats_Actin["Actin_chromo_volume_T1"].append(None)
+    #     stats_Actin["Actin_chromo_intensity_T1"].append(None)   
 
-for temp_read in exactChromoFiles:
-    df = pd.read_excel(temp_read)        
-    stats_Actin["Actin_chromo_volume_T1"].append(df.loc[0, 'Chromocenter Area'])
-    stats_Actin["Actin_chromo_intensity_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])             
-
-# Actin Read
-for temp_read in exactActinFiles:
-    df = pd.read_excel(temp_read)        
-    stats_Actin["Actin_coverage_T30"].append(df.loc[0,'Actin Coverage'])     
-  
-     
+# Actin Read T30
+for actinread in ActinFilesT1:
+    file_name = actinread.split("(Actin)_")[1].split(".")[0].strip()
+    file_name = file_name.replace("T30", "T0") 
+    if file_name in stats_Actin["File_Name_Actin"]:
+        df = pd.read_excel(actinread)        
+        stats_Actin["Actin_coverage_T30"].append(df.loc[0,'Actin Coverage']) 
+    # else:        
+    #     stats_Actin["Actin_coverage_T30"].append(None) 
+        
 df_Actin = pd.DataFrame.from_dict(stats_Actin, orient='index')
-df_Actin = df_Actin.transpose()   
+df_Actin = df_Actin.transpose()         
 
+###########################
+#    Ctrl    Reading      #
+###########################
+
+directory = folder_Path + 'T0/Result/Ctrl/'
+
+ChromoFilesT0 = folder_scan(directory, ".xlsx", marker="(Chromo)_")
+NucleusFilesT0 = folder_scan(directory, ".xlsx", marker="(Nucleus)_")
+
+directory = folder_Path + 'T30/Result/Ctrl/'
+
+ChromoFilesT1 = folder_scan(directory, ".xlsx", marker="(Chromo)_")
+NucleusFilesT1 = folder_scan(directory, ".xlsx", marker="(Nucleus)_")
+
+# Ctrl Nucleus Read T0
+for nucleusread in NucleusFilesT0:
+    df = pd.read_excel(nucleusread)
+    file_name = nucleusread.split("(Nucleus)_")[1].split(".")[0].strip()
+    stats_Ctrl["File_Name_Ctrl"].append(file_name)
+    stats_Ctrl["Nuclei_volume_ctrl_T0"].append(df.loc[0,'Nucleus Area'])
+    stats_Ctrl["Nuclei_intensity_ctrl_T0"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
+
+# Ctrl Nucleus Read T30        
+for nucleusread in NucleusFilesT1:
+    file_name = nucleusread.split("(Nucleus)_")[1].split(".")[0].strip()
+    file_name = file_name.replace("T30", "T0")
+    if file_name in stats_Ctrl["File_Name_Ctrl"]:    
+        df = pd.read_excel(nucleusread)
+        #file_name = nucleusread.split("(Nucleus)_")[1].split(".")[0].strip()
+        #stats_Actin["File_Name_Actin"].append(file_name)
+        stats_Ctrl["Nuclei_volume_ctrl_T1"].append(df.loc[0,'Nucleus Area'])
+        stats_Ctrl["Nuclei_intensity_ctrl_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])
+        # else:
+           
+# Chromocenter Read T0
+for temp_read in ChromoFilesT0:
+    df = pd.read_excel(temp_read)        
+    stats_Ctrl["Ctrl_chromo_volume_T0"].append(df.loc[0, 'Chromocenter Area'])
+    stats_Ctrl["Ctrl_chromo_intensity_T0"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])     
+
+# Chromocenter Read T30
+for chromo_read in ChromoFilesT1:
+    file_name = chromo_read.split("(Chromo)_")[1].split(".")[0].strip()
+    file_name = file_name.replace("T30", "T0")   
+    if file_name in stats_Ctrl["File_Name_Ctrl"]:    
+        df = pd.read_excel(chromo_read)        
+        stats_Ctrl["Ctrl_chromo_volume_T1"].append(df.loc[0, 'Chromocenter Area'])
+        stats_Ctrl["Ctrl_chromo_intensity_T1"].append(df.iloc[0, df.columns.get_loc('mean_intensity')])             
+     
+df_Ctrl = pd.DataFrame.from_dict(stats_Ctrl, orient='index')
+df_Ctrl = df_Ctrl.transpose()  
 # =============================================================================
 # # Filter out the incorrectly Segmented Chromo on T0
 # filterChromo = df_Actin['Actin_chromo_volume_T1'] > 25
